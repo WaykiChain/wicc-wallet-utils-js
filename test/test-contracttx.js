33fa9b469e1aaf3a68e7f7854644108b45aa176d
chenxinhua2018 <15220088021@163.com>
2019-01-26 15:46:41 +0800

    Tidy the test code

--- test/test-contracttx.js
+++ test/test-contracttx.js
@@ -1,25 +1,12 @@
 'use strict';
 
-// usage: node test-registerapptx.js
+// usage: node test-contracttx.js
 
 var bitcore = require('..');
-
-var fs = require("fs")
-
-
-//var privateKey = new bitcore.PrivateKey(null, 'testnet');
-var privateKey = bitcore.PrivateKey.fromWIF('Y8JhshTg5j2jeTrwk3qBJDYGi5MVsAvfBJRgFfAp14T91UY9AHgZ')
-var address = privateKey.toAddress();
-var publicKey = privateKey.toPublicKey();
-
 
 
 var arg = {network: 'testnet'}
 var wiccApi = new bitcore.WiccApi(arg)
-
-var script = fs.readFileSync(__dirname + '/data/contract-hello.lua');
-//console.log("load script file:")
-//console.log(script);
 
 
 
@@ -27,62 +14,15 @@ var script = fs.readFileSync(__dirname + '/data/contract-hello.lua');
 var regAppInfo = {
   nTxType: bitcore.WiccApi.CONTRACT_TX,
   nVersion: 1,
-  nValidHeight: 34400,        // create height
+  nValidHeight: 34400,    // create height
   srcRegId: "22030-2",    // sender's regId
   destRegId: "24555-1",  // app regId
   fees: 1000000,         // fees pay for miner
   value: 8,              // amount of WICC to be sent to the app account
-  vContract: "f018"
+  vContract: "f018"      // contract method, hex format string
 };
 
-var str=regAppInfo.vContract
-var all = parseInt("0x" + str)
-console.log("all="+all)
-var a = parseInt("0x" + str.substr(0, 2))
-var b = parseInt("0x" + str.substr(2, 2))
-console.log("ab = " +a+""+b)
-
-/*
-  "1.\"senderaddr\": (string, required)\n tx sender's base58 addr\n"
-  "2.\"appregid\":(string, required) the app RegId\n"
-  "3.\"amount\":(numeric, required)\n amount of WICC to be sent to the app account\n"
-  "4.\"contract\": (string, required) contract method invoke content (Hex encode required)\n"
-  "5.\"fee\": (numeric, required) pay to miner\n"
-  "6.\"height\": (numeric, optional)create height,If not provide use the tip block hegiht in chainActive\n"
-*/
-
-/*  
-{
-    "hash" : "dd4da1cd31f6d6eb8f81c067f5ac35c2f7334c47713f09670be5b5c9fd9887c2",
-    "txtype" : "REG_APP_TX",
-    "ver" : 1,
-    "regid" : "22030-2",
-    "addr" : "waFdYrQfDSsh1jVsDzPiutAPUKCHZJ4Wyp",
-    "script" : "script_content",
-    "fees" : 110000000,
-    "height" : 22039,
-    "blockhash" : "d0e6be692161da4367d25258c535d4cd8a2969f4642b018e9c2939dce21ad6b5",
-    "confirmHeight" : 22041,
-    "confirmedtime" : 1548295400,
-    "rawtx" : "050180ab170480ab0e02fd4704fd43046d796c6962203d207265717569726520226d796c6962220a2d2de8849ae69cace4b8ade5bf85e9a1bbe4bba5606d796c6962203d207265717569726520226d796c69622260e5bc80e5a4b4efbc8ce6b3a8e6848fe4b880e5ae9ae8a681e694bee59ca8e7acace4b880e8a18cefbc8ce7acace4b880e8a18ce5a682e69e9ce79599e7a9bae4bc9ae68aa5e5bc82e5b8b8e380820a0a2d2de5ae9ae4b989e59088e7baa6e8b083e794a8e4ba8be4bbb60a4d4554484f44203d207b0a20202020434845434b5f48454c4c4f574f524c4420203d20307831372c0a2020202053454e445f48454c4c4f574f524c44203d20307831380a7d0a0a2d2de58f82e880835b342e3320415049e8b083e8af95e696b9e6b395e5ae9ee4be8b5d0a2d2de794a8e4ba8ee8be93e587ba6c6f67e4bfa1e681afe887b3e69687e4bbb60a4c6f674d7367203d2066756e6374696f6e20286d7367290a2020206c6f63616c206c6f675461626c65203d207b0a20202020202020206b6579203d20302c0a20202020202020206c656e677468203d20737472696e672e6c656e286d7367292c0a202020202020202076616c7565203d206d73670a20207d0a20206d796c69622e4c6f675072696e74286c6f675461626c65290a656e640a0a2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d0a0a436865636b203d2066756e6374696f6e28290a202020204c6f674d7367282252756e20434845434b5f48454c4c4f574f524c44204d6574686f6422290a656e640a0a53656e64203d2066756e6374696f6e28290a202020204c6f674d7367282252756e2053454e445f48454c4c4f574f524c44204d6574686f6422290a656e640a0a2d2de58f82e880835b342e32e5bc80e58f91e5b8b8e794a8e696b9e6b3955d0a2d2de699bae883bde59088e7baa6e585a5e58fa30a4d61696e203d2066756e6374696f6e28290a20206173736572742823636f6e7472616374203e3d322c2022506172616d206c656e677468206572726f7220283c32293a2022202e2e23636f6e747261637420290a202061737365727428636f6e74726163745b315d203d3d20307866302c2022506172616d204d616769634e6f206572726f7220287e3d30786630293a2022202e2e20636f6e74726163745b315d290a0a2020696620636f6e74726163745b325d203d3d204d4554484f442e434845434b5f48454c4c4f574f524c44207468656e0a20202020436865636b28290a2020656c7365696620636f6e74726163745b325d203d3d204d4554484f442e53454e445f48454c4c4f574f524c44207468656e0a2020202053656e6428290a2020656c73650a202020206572726f7228276d6574686f642320272e2e737472696e672e666f726d6174282225303278222c20636f6e74726163745b325d292e2e27206e6f7420666f756e6427290a2020656e640a656e640a0a4d61696e28290a0a00b3b8ee00473045022100af11d737ae539981bfaeeba0bd6da07ef5da258b68d09c3df2bc8bf4aaec16f1022050e9279b481b3747a203126ccef08959448e9d8acacc0695f8f4792f91acac5a"
-}
-
-*/
-
-var wicc_private_key='Y9f6JFRnYkHMPuEhymC15wHD9FbYBmeV2S6VfDicb4ghNhtXhgAJ'
-//var addr='waFdYrQfDSsh1jVsDzPiutAPUKCHZJ4Wyp'
-
-//var strMne = wiccApi.createAllCoinMnemonicCode()
-//console.log("new mnemonic code:")
-//console.log(strMne)
-
-//var walletinfo = wiccApi.createWallet(strMne, '12345678')
-//console.log("new wallet info:")
-//console.log(walletinfo)
-
-
-//var wiccPrivateKey = wiccApi.getPriKeyFromSeed(walletinfo.seedinfo, '12345678')
-var wiccPrivateKey = wicc_private_key
+var wiccPrivateKey = 'Y9f6JFRnYkHMPuEhymC15wHD9FbYBmeV2S6VfDicb4ghNhtXhgAJ'
 console.log("wicc private key:")
 console.log(wiccPrivateKey)
 
--- test/test-registerapptx.js
+++ test/test-registerapptx.js
@@ -1,13 +1,11 @@
 'use strict';
 
-// usage: node test-registerapptx.js
+// usage: 
+// node test-registerapptx.js
 
 var _ = require('lodash');
 var bitcore = require('..');
 var fs = require("fs")
-
-//var privateKey = new bitcore.PrivateKey(null, 'testnet');
-var privateKey = bitcore.PrivateKey.fromWIF('Y8JhshTg5j2jeTrwk3qBJDYGi5MVsAvfBJRgFfAp14T91UY9AHgZ')
 
 var arg = {network: 'testnet'}
 var wiccApi = new bitcore.WiccApi(arg)
@@ -25,51 +23,20 @@ console.log(_.isBuffer(script))
 var regAppInfo = {
     nTxType: bitcore.WiccApi.REG_APP_TX,
     nVersion: 1,
-    nValidHeight: 34400,
-    regAcctId: "22030-2",
-    script: script,
-    scriptDesc: "",
-    fees: 110000000,
+    nValidHeight: 34400,       // create height
+    regAcctId: "22030-2",      // sender's regId
+    script: script,            // contract scrypt content, string or buf
+    scriptDesc: "",            // contract scrypt description, string or buf
+    fees: 110000000,           // fees pay for miner
   };
 
-/*  
-{
-    "hash" : "dd4da1cd31f6d6eb8f81c067f5ac35c2f7334c47713f09670be5b5c9fd9887c2",
-    "txtype" : "REG_APP_TX",
-    "ver" : 1,
-    "regid" : "22030-2",
-    "addr" : "waFdYrQfDSsh1jVsDzPiutAPUKCHZJ4Wyp",
-    "script" : "script_content",
-    "fees" : 110000000,
-    "height" : 22039,
-    "blockhash" : "d0e6be692161da4367d25258c535d4cd8a2969f4642b018e9c2939dce21ad6b5",
-    "confirmHeight" : 22041,
-    "confirmedtime" : 1548295400,
-    "rawtx" : "050180ab170480ab0e02fd4704fd43046d796c6962203d207265717569726520226d796c6962220a2d2de8849ae69cace4b8ade5bf85e9a1bbe4bba5606d796c6962203d207265717569726520226d796c69622260e5bc80e5a4b4efbc8ce6b3a8e6848fe4b880e5ae9ae8a681e694bee59ca8e7acace4b880e8a18cefbc8ce7acace4b880e8a18ce5a682e69e9ce79599e7a9bae4bc9ae68aa5e5bc82e5b8b8e380820a0a2d2de5ae9ae4b989e59088e7baa6e8b083e794a8e4ba8be4bbb60a4d4554484f44203d207b0a20202020434845434b5f48454c4c4f574f524c4420203d20307831372c0a2020202053454e445f48454c4c4f574f524c44203d20307831380a7d0a0a2d2de58f82e880835b342e3320415049e8b083e8af95e696b9e6b395e5ae9ee4be8b5d0a2d2de794a8e4ba8ee8be93e587ba6c6f67e4bfa1e681afe887b3e69687e4bbb60a4c6f674d7367203d2066756e6374696f6e20286d7367290a2020206c6f63616c206c6f675461626c65203d207b0a20202020202020206b6579203d20302c0a20202020202020206c656e677468203d20737472696e672e6c656e286d7367292c0a202020202020202076616c7565203d206d73670a20207d0a20206d796c69622e4c6f675072696e74286c6f675461626c65290a656e640a0a2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d0a0a436865636b203d2066756e6374696f6e28290a202020204c6f674d7367282252756e20434845434b5f48454c4c4f574f524c44204d6574686f6422290a656e640a0a53656e64203d2066756e6374696f6e28290a202020204c6f674d7367282252756e2053454e445f48454c4c4f574f524c44204d6574686f6422290a656e640a0a2d2de58f82e880835b342e32e5bc80e58f91e5b8b8e794a8e696b9e6b3955d0a2d2de699bae883bde59088e7baa6e585a5e58fa30a4d61696e203d2066756e6374696f6e28290a20206173736572742823636f6e7472616374203e3d322c2022506172616d206c656e677468206572726f7220283c32293a2022202e2e23636f6e747261637420290a202061737365727428636f6e74726163745b315d203d3d20307866302c2022506172616d204d616769634e6f206572726f7220287e3d30786630293a2022202e2e20636f6e74726163745b315d290a0a2020696620636f6e74726163745b325d203d3d204d4554484f442e434845434b5f48454c4c4f574f524c44207468656e0a20202020436865636b28290a2020656c7365696620636f6e74726163745b325d203d3d204d4554484f442e53454e445f48454c4c4f574f524c44207468656e0a2020202053656e6428290a2020656c73650a202020206572726f7228276d6574686f642320272e2e737472696e672e666f726d6174282225303278222c20636f6e74726163745b325d292e2e27206e6f7420666f756e6427290a2020656e640a656e640a0a4d61696e28290a0a00b3b8ee00473045022100af11d737ae539981bfaeeba0bd6da07ef5da258b68d09c3df2bc8bf4aaec16f1022050e9279b481b3747a203126ccef08959448e9d8acacc0695f8f4792f91acac5a"
-}
-
-*/
-
-var wicc_private_key='Y9f6JFRnYkHMPuEhymC15wHD9FbYBmeV2S6VfDicb4ghNhtXhgAJ'
-//var addr='waFdYrQfDSsh1jVsDzPiutAPUKCHZJ4Wyp'
-
-//var strMne = wiccApi.createAllCoinMnemonicCode()
-//console.log("new mnemonic code:")
-//console.log(strMne)
-
-//var walletinfo = wiccApi.createWallet(strMne, '12345678')
-//console.log("new wallet info:")
-//console.log(walletinfo)
-
-
-//var wiccPrivateKey = wiccApi.getPriKeyFromSeed(walletinfo.seedinfo, '12345678')
-var wiccPrivateKey = wicc_private_key
+var wiccPrivateKey = 'Y9f6JFRnYkHMPuEhymC15wHD9FbYBmeV2S6VfDicb4ghNhtXhgAJ'
 console.log("wicc private key:")
 console.log(wiccPrivateKey)
 
 var privateKey = bitcore.PrivateKey.fromWIF(wiccPrivateKey)
-console.log("get private key:")
-console.log(privateKey)
+//console.log("get private key:")
+//console.log(privateKey)
 var address = privateKey.toAddress();
 console.log("get address:")
 console.log(address.toString())
