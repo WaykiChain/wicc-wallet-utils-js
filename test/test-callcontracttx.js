a7851f46296c003380cf0942b0b613e51e6ff2de
jk <327413510@qq.com>
2019-04-20 19:44:22 +0800

    Organize the guidance document

--- test/test-callcontracttx.js
+++ test/test-callcontracttx.js
@@ -4,13 +4,23 @@
 
 var bitcore = require('..');
 
-
 var arg = {network: 'testnet'}
 var wiccApi = new bitcore.WiccApi(arg)
 
-
-
-//note: change "nValidHeight" to current valid height, so that you can execute “submittx” ok after get the result
+/*
+Build a transaction for calling smart contract
+note:
+1, nValidHeight: the height of the block when creating the signature, and the height difference when submitting the broadcast transaction must be <=250
+2, fees: handling fee when calling smart contract , >= 100000 sawi (0.001 wicc)
+3. The same transaction cannot be submitted repeatedly before it is confirmed(BPS=0.1). It is recommended to solve the problem of batch initiated transaction by adding random handling fee.
+*/
+/*
+构建调用合约的交易单
+注意：
+1、nValidHeight:创建签名时的区块高度,与提交广播交易时的高度差必须 <=250
+2、fees:调用合约交易时的手续费, >= 100000 sawi(0.001 wicc)
+3、相同的交易在未被确认前不能重复提交(BPS=0.1),建议采用添加随机手续费方式解决批量发起交易问题
+*/
 var regAppInfo = {
   nTxType: bitcore.WiccApi.CONTRACT_TX,
   nVersion: 1,
---
+++ test/test-wallet.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var bitcore = require('..');
+
+//environment init 
+//环境初始化
+var arg = {network: 'testnet'}
+var wiccApi = new bitcore.WiccApi(arg)
+
+//Create Mnemonic Code
+//创建助记词
+var strMne = wiccApi.createAllCoinMnemonicCode()
+console.log('New MnemonicCode='+ strMne)
+
+//Check if the mnemonic is valid
+//检查助记词是否有效
+var ret = wiccApi.checkMnemonicCode(strMne)
+console.log('Check MnemonicCode Result=' + ret)
+
+//Get WIF privatekey from mnemoniccode
+//根据助记词获取对应的WIF格式私钥
+var privateKey1 = wiccApi.getPriKeyFromMnemonicCode(strMne)
+console.log('privateKey1='+privateKey1)
+
+//Create a wallet based on the mnemonic, '12345678' is the wallet password
+//根据助记词创建钱包,'12345678'为钱包密码
+var walletInfo = wiccApi.createWallet(strMne, '12345678')
+console.log('walletInfo=')
+console.log(walletInfo)
+
+//Get WIF privatekey from wallet
+//根据钱包种子获取对应的WIF格式私钥
+var privateKey2 = wiccApi.getPriKeyFromSeed(walletInfo.seedinfo, '12345678')
+console.log('privateKey2='+privateKey2)
+
+//Get mnemonics based on wallet seeds
+//根据钱包种子获取助记词
+var Mne = wiccApi.getMnemonicCodeFromSeed(walletInfo.seedinfo, '12345678')
+console.log("Get MnemonicCode from wallet seed="+Mne)
+
+//Get an address based on the WIF format private key. Note: privateKey2 and privateKey1 are equal.
+//根据WIF格式私钥获取地址 ,注意：privateKey2 和 privateKey1 是一样的
+var privateKey = bitcore.PrivateKey.fromWIF(privateKey2)
+var address = privateKey.toAddress();
+console.log("New Create address="+address.toString())
+
+//Check if the address is valid
+//检查地址是否有效
+ret = wiccApi.validateAddress(address)
+console.log("Check address Result="+ret)
+
