c17f8a9956a435eeef6fdcbdf24e055a361d3636
彭鹏 <peng6907743@163.com>
2019-12-18 14:40:14 +0800

    update

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ var WalletManager = require("./src/WalletManager")
 var WaykiTransaction = require('./src/WaykiTransaction')
 var BaasClient = require('./src/BaasClient')
 
-WiccApi = Object.assign(WiccApi, bitcore)
-
 WiccWalletLib.Wallet = Wallet
 WiccWalletLib.WalletManager = WalletManager
 WiccWalletLib.WaykiTransaction = WaykiTransaction
 WiccWalletLib.WiccApi = WiccApi
+WiccWalletLib.bitcore = bitcore
 WiccWalletLib.BaasClient = BaasClient
 
 module.exports = WiccWalletLib
