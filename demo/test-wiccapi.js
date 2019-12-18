3cf18e394d1f3d6ce3a440d7e531b18868d7f447
彭鹏 <peng6907743@163.com>
2019-12-18 14:44:48 +0800

    update

--- demo/test-wiccapi.js
+++ demo/test-wiccapi.js
@@ -1,6 +1,6 @@
 'use strict';
 console.error('\n=====RUN-TEST-WICCAPI-START=====\n')
-var  { WiccApi } = require("../index")
+var  { WiccApi, bitcore } = require("../index")
 
 //environment init 
 //环境初始化
@@ -45,7 +45,7 @@ console.log("Get MnemonicCode from wallet seed="+Mne)
 
 //Get an address based on the WIF format private key. Note: privateKey2 and privateKey1 are equal.
 //根据WIF格式私钥获取地址 ,注意：privateKey2 和 privateKey1 是一样的
-var privateKey = WiccApi.PrivateKey.fromWIF(privateKey2)
+var privateKey = bitcore.PrivateKey.fromWIF(privateKey2)
 var pubKey=privateKey.toPublicKey();
 var address = privateKey.toAddress();
 console.log("New Create publicKey1="+pubKey,"New Create address="+address.toString())
